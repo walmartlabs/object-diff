d2e4164f1964ac40d943cf89eb4fbfa40b85778a
Nik Hodgkinson <Nicholas.Hodgkinson@walmartlabs.com>
2018-11-26 18:01:49 -0600

    Added LICENSE and headers.

--- pkg/obj_diff/testing_types.go
+++ pkg/obj_diff/testing_types.go
@@ -1,3 +1,7 @@
+// Copyright (c) Walmart Inc.
+//
+// This source code is licensed under the Apache 2.0 license found in the
+// LICENSE file in the root directory of this source tree.
 package obj_diff
 
 type structInt32 struct {
